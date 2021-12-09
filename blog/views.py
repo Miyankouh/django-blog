249c1c681d84d6b425af7874fc9aa816b8b90b3b
Miyankouh <majid.miyankouh@gmail.com>
2021-12-09 13:13:18 +0330

    Paginator

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse, Http404
 from .models import Article, Category
@@ -7,8 +8,12 @@ from .models import Article, Category
 #----------------------------------------------------------------
 
 def home(request):
+    articles_list = Article.objects.published()
+    paginator = Paginator(articles_list, 5)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
     context = {
-       "articles": Article.objects.published(),
+       "articles": articles,
       
 }
     return render(request, "blog/home.html", context)
